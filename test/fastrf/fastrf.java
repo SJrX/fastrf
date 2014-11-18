790085890935ff1d5885de5001e6300acd0605cc
Marius Lindauer <marius.rks@googlemail.com>
2014-11-18 13:30:27 +0100

    adding more unit tests

--- test/fastrf/fastrf.java
+++ test/fastrf/fastrf.java
@@ -99,7 +99,444 @@ public class fastrf {
 		double[][] pred = RandomForest.apply(rf, x);
 		System.out.println(Arrays.deepToString(pred));
 		//fail("Not yet implemented");
+	}
+	
+
+	@Test
+	/*
+	 * test EQ by adding a "==" condition that disables parameter "0" always;
+	 * since "0" is the only informative variable, always the same value should be predicted 
+	 */
+	public void testEQContNonActive() {
+		RegtreeBuildParams buildparams = new RegtreeBuildParams();
+		buildparams.random = new Random(3);
+		buildparams.splitMin = 1;
+		//theta 0 has two categorical values: 1,2; all others are continuous
+		int[] catDomainSizes = {2,0,0};
+		buildparams.catDomainSizes = catDomainSizes;
+		buildparams.minVariance = Math.pow(10,-14);
+		buildparams.ratioFeatures = 1;
 		
+		buildparams.nameConditionsMapOp = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsArray = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsValues = new HashMap<Integer, double[][][]>();
+		
+		// "0" | "1" == 1.0
+		int[][] op = {{0}}; //EQ
+		buildparams.nameConditionsMapOp.put(0, op);
+		int[][] par = {{1}}; //Parent first theta
+		buildparams.nameConditionsMapParentsArray.put(0, par);
+		double[][][] val = {{{1.}}}; //value 1
+		buildparams.nameConditionsMapParentsValues.put(0, val);
+		
+		int numTrees = 10;
+		double[][] allTheta = {{1.,0.}, {2.,0.} };
+		
+		//no features
+		double[][] allX = {{0}, {0}};
+		int[][] theta_inst_idxs = {{0,0},{1,1},{0,1},{1,0}};
+		double[] y = {4., 1., 5., 2.};
+				
+	
+		RandomForest rf = RandomForest.learnModel(numTrees, allTheta, allX, theta_inst_idxs, y, buildparams);
+	
+		double[][] x = {{1.,1.,0.,}, {2.,0.,0.}, {1.,1.,1.}};
+		double[][] pred = RandomForest.apply(rf, x);
+		System.out.println("EQ NonActive Test:");
+		System.out.println(Arrays.deepToString(pred));
+		//fail("Not yet implemented");
+		
+		assertTrue(pred[0][0] == 3.175);
+		assertTrue(pred[1][0] == 3.175);
+		assertTrue(pred[2][0] == 3.175);
+	}
+	
+	@Test
+	/*
+	 * test NEQ by adding a "!=" condition that does nothing;
+	 */
+	public void testNEQCont() {
+		RegtreeBuildParams buildparams = new RegtreeBuildParams();
+		buildparams.random = new Random(3);
+		buildparams.splitMin = 1;
+		//theta 0 has two categorical values: 1,2; all others are continuous
+		int[] catDomainSizes = {2,0,0};
+		buildparams.catDomainSizes = catDomainSizes;
+		buildparams.minVariance = Math.pow(10,-14);
+		buildparams.ratioFeatures = 1;
+		
+		buildparams.nameConditionsMapOp = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsArray = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsValues = new HashMap<Integer, double[][][]>();
+		
+		// "0" | "1" != 2.0
+		int[][] op = {{1}}; //NEQ
+		buildparams.nameConditionsMapOp.put(0, op);
+		int[][] par = {{1}}; //Parent first theta
+		buildparams.nameConditionsMapParentsArray.put(0, par);
+		double[][][] val = {{{2.}}}; //value 0
+		buildparams.nameConditionsMapParentsValues.put(0, val);
+		
+		int numTrees = 10;
+		double[][] allTheta = {{1.,0.}, {2.,0.} };
+		
+		//no features
+		double[][] allX = {{0}, {0}};
+		int[][] theta_inst_idxs = {{0,0},{1,1},{0,1},{1,0}};
+		double[] y = {4., 1., 5., 2.};
+				
+	
+		RandomForest rf = RandomForest.learnModel(numTrees, allTheta, allX, theta_inst_idxs, y, buildparams);
+	
+		double[][] x = {{1.,1.,0.,}, {2.,0.,0.}, {1.,1.,1.}};
+		double[][] pred = RandomForest.apply(rf, x);
+		System.out.println("NEQ Test:");
+		System.out.println(Arrays.deepToString(pred));
+		//fail("Not yet implemented");
+		
+		assertTrue(pred[0][0] > 4.38);
+		assertTrue(pred[1][0] == 1.7);
+		assertTrue(pred[2][0] > 4.38);
+	}
+	
+	@Test
+	/*
+	 * test NEQ by adding a "!=" condition that disables parameter "0" always;
+	 * since "0" is the only informative variable, always the same value should be predicted 
+	 */
+	public void testNEQContNonActive() {
+		RegtreeBuildParams buildparams = new RegtreeBuildParams();
+		buildparams.random = new Random(3);
+		buildparams.splitMin = 1;
+		//theta 0 has two categorical values: 1,2; all others are continuous
+		int[] catDomainSizes = {2,0,0};
+		buildparams.catDomainSizes = catDomainSizes;
+		buildparams.minVariance = Math.pow(10,-14);
+		buildparams.ratioFeatures = 1;
+		
+		buildparams.nameConditionsMapOp = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsArray = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsValues = new HashMap<Integer, double[][][]>();
+		
+		// "0" | "1" != 0.0
+		int[][] op = {{1}}; //NEQ
+		buildparams.nameConditionsMapOp.put(0, op);
+		int[][] par = {{1}}; //Parent first theta
+		buildparams.nameConditionsMapParentsArray.put(0, par);
+		double[][][] val = {{{0.}}}; //value 0
+		buildparams.nameConditionsMapParentsValues.put(0, val);
+		
+		int numTrees = 10;
+		double[][] allTheta = {{1.,0.}, {2.,0.} };
+		
+		//no features
+		double[][] allX = {{0}, {0}};
+		int[][] theta_inst_idxs = {{0,0},{1,1},{0,1},{1,0}};
+		double[] y = {4., 1., 5., 2.};
+				
+	
+		RandomForest rf = RandomForest.learnModel(numTrees, allTheta, allX, theta_inst_idxs, y, buildparams);
+	
+		double[][] x = {{1.,1.,0.,}, {2.,0.,0.}, {1.,1.,1.}};
+		double[][] pred = RandomForest.apply(rf, x);
+		System.out.println("NEQ NonActive Test:");
+		System.out.println(Arrays.deepToString(pred));
+		//fail("Not yet implemented");
+		
+		assertTrue(pred[0][0] == 3.175);
+		assertTrue(pred[1][0] == 3.175);
+		assertTrue(pred[2][0] == 3.175);
+	}
+	
+	@Test
+	/*
+	 * test LE by adding a "<" condition that does nothing;
+	 */
+	public void testLECont() {
+		RegtreeBuildParams buildparams = new RegtreeBuildParams();
+		buildparams.random = new Random(3);
+		buildparams.splitMin = 1;
+		//theta 0 has two categorical values: 1,2; all others are continuous
+		int[] catDomainSizes = {2,0,0};
+		buildparams.catDomainSizes = catDomainSizes;
+		buildparams.minVariance = Math.pow(10,-14);
+		buildparams.ratioFeatures = 1;
+		
+		buildparams.nameConditionsMapOp = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsArray = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsValues = new HashMap<Integer, double[][][]>();
+		
+		// "0" | "1" < 1.0
+		int[][] op = {{2}}; //LE
+		buildparams.nameConditionsMapOp.put(0, op);
+		int[][] par = {{1}}; //Parent first theta
+		buildparams.nameConditionsMapParentsArray.put(0, par);
+		double[][][] val = {{{1.}}}; //value 0
+		buildparams.nameConditionsMapParentsValues.put(0, val);
+		
+		int numTrees = 10;
+		double[][] allTheta = {{1.,0.}, {2.,0.} };
+		
+		//no features
+		double[][] allX = {{0}, {0}};
+		int[][] theta_inst_idxs = {{0,0},{1,1},{0,1},{1,0}};
+		double[] y = {4., 1., 5., 2.};
+				
+	
+		RandomForest rf = RandomForest.learnModel(numTrees, allTheta, allX, theta_inst_idxs, y, buildparams);
+	
+		double[][] x = {{1.,1.,0.,}, {2.,0.,0.}, {1.,1.,1.}};
+		double[][] pred = RandomForest.apply(rf, x);
+		System.out.println("LE Test:");
+		System.out.println(Arrays.deepToString(pred));
+		//fail("Not yet implemented");
+		
+		assertTrue(pred[0][0] > 4.38);
+		assertTrue(pred[1][0] == 1.7);
+		assertTrue(pred[2][0] > 4.38);
+	}
+	
+	@Test
+	/*
+	 * test LE by adding a "<" condition that disables parameter "0" always;
+	 * since "0" is the only informative variable, always the same value should be predicted 
+	 */
+	public void testLEContNonActive() {
+		RegtreeBuildParams buildparams = new RegtreeBuildParams();
+		buildparams.random = new Random(3);
+		buildparams.splitMin = 1;
+		//theta 0 has two categorical values: 1,2; all others are continuous
+		int[] catDomainSizes = {2,0,0};
+		buildparams.catDomainSizes = catDomainSizes;
+		buildparams.minVariance = Math.pow(10,-14);
+		buildparams.ratioFeatures = 1;
+		
+		buildparams.nameConditionsMapOp = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsArray = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsValues = new HashMap<Integer, double[][][]>();
+		
+		// "0" | "1" < 0.0
+		int[][] op = {{2}}; //LE
+		buildparams.nameConditionsMapOp.put(0, op);
+		int[][] par = {{1}}; //Parent first theta
+		buildparams.nameConditionsMapParentsArray.put(0, par);
+		double[][][] val = {{{0.}}}; //value 0
+		buildparams.nameConditionsMapParentsValues.put(0, val);
+		
+		int numTrees = 10;
+		double[][] allTheta = {{1.,0.}, {2.,0.} };
+		
+		//no features
+		double[][] allX = {{0}, {0}};
+		int[][] theta_inst_idxs = {{0,0},{1,1},{0,1},{1,0}};
+		double[] y = {4., 1., 5., 2.};
+				
+	
+		RandomForest rf = RandomForest.learnModel(numTrees, allTheta, allX, theta_inst_idxs, y, buildparams);
+	
+		double[][] x = {{1.,1.,0.,}, {2.,0.,0.}, {1.,1.,1.}};
+		double[][] pred = RandomForest.apply(rf, x);
+		System.out.println("LE NonActive Test:");
+		System.out.println(Arrays.deepToString(pred));
+		//fail("Not yet implemented");
+		
+		assertTrue(pred[0][0] == 3.175);
+		assertTrue(pred[1][0] == 3.175);
+		assertTrue(pred[2][0] == 3.175);
+	}
+	
+	@Test
+	/*
+	 * test GR by adding a ">" condition that does nothing
+	 */
+	public void testGRCont() {
+		RegtreeBuildParams buildparams = new RegtreeBuildParams();
+		buildparams.random = new Random(3);
+		buildparams.splitMin = 1;
+		//theta 0 has two categorical values: 1,2; all others are continuous
+		int[] catDomainSizes = {2,0,0};
+		buildparams.catDomainSizes = catDomainSizes;
+		buildparams.minVariance = Math.pow(10,-14);
+		buildparams.ratioFeatures = 1;
+		
+		buildparams.nameConditionsMapOp = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsArray = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsValues = new HashMap<Integer, double[][][]>();
+		
+		// "0" | "1" > -0.5
+		int[][] op = {{3}}; //GR
+		buildparams.nameConditionsMapOp.put(0, op);
+		int[][] par = {{1}}; //Parent first theta
+		buildparams.nameConditionsMapParentsArray.put(0, par);
+		double[][][] val = {{{-0.5}}}; //value 1
+		buildparams.nameConditionsMapParentsValues.put(0, val);
+		
+		int numTrees = 10;
+		double[][] allTheta = {{1.,0.}, {2.,0.} };
+		
+		//no features
+		double[][] allX = {{0}, {0}};
+		int[][] theta_inst_idxs = {{0,0},{1,1},{0,1},{1,0}};
+		double[] y = {4., 1., 5., 2.};
+				
+	
+		RandomForest rf = RandomForest.learnModel(numTrees, allTheta, allX, theta_inst_idxs, y, buildparams);
+	
+		double[][] x = {{1.,1.,0.,}, {2.,0.,0.}, {1.,1.,1.}};
+		double[][] pred = RandomForest.apply(rf, x);
+		System.out.println("GR Test:");
+		System.out.println(Arrays.deepToString(pred));
+		//fail("Not yet implemented");
+		
+		assertTrue(pred[0][0] > 4.38);
+		assertTrue(pred[1][0] == 1.7);
+		assertTrue(pred[2][0] > 4.38);
+	}
+	
+	@Test
+	/*
+	 * test GR by adding a ">" condition that disables parameter "0" always;
+	 * since "0" is the only informative variable, always the same value should be predicted 
+	 */
+	public void testGRContNonActive() {
+		RegtreeBuildParams buildparams = new RegtreeBuildParams();
+		buildparams.random = new Random(3);
+		buildparams.splitMin = 1;
+		//theta 0 has two categorical values: 1,2; all others are continuous
+		int[] catDomainSizes = {2,0,0};
+		buildparams.catDomainSizes = catDomainSizes;
+		buildparams.minVariance = Math.pow(10,-14);
+		buildparams.ratioFeatures = 1;
+		
+		buildparams.nameConditionsMapOp = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsArray = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsValues = new HashMap<Integer, double[][][]>();
+		
+		// "0" | "1" > 1.0
+		int[][] op = {{3}}; //GR
+		buildparams.nameConditionsMapOp.put(0, op);
+		int[][] par = {{1}}; //Parent first theta
+		buildparams.nameConditionsMapParentsArray.put(0, par);
+		double[][][] val = {{{1.}}}; //value 1
+		buildparams.nameConditionsMapParentsValues.put(0, val);
+		
+		int numTrees = 10;
+		double[][] allTheta = {{1.,0.}, {2.,0.} };
+		
+		//no features
+		double[][] allX = {{0}, {0}};
+		int[][] theta_inst_idxs = {{0,0},{1,1},{0,1},{1,0}};
+		double[] y = {4., 1., 5., 2.};
+				
+	
+		RandomForest rf = RandomForest.learnModel(numTrees, allTheta, allX, theta_inst_idxs, y, buildparams);
+	
+		double[][] x = {{1.,1.,0.,}, {2.,0.,0.}, {1.,1.,1.}};
+		double[][] pred = RandomForest.apply(rf, x);
+		System.out.println("GR NonActive Test:");
+		System.out.println(Arrays.deepToString(pred));
+		//fail("Not yet implemented");
+		
+		assertTrue(pred[0][0] == 3.175);
+		assertTrue(pred[1][0] == 3.175);
+		assertTrue(pred[2][0] == 3.175);
+	}
+	
+	@Test
+	/*
+	 * test IN by adding a "in" condition that disables parameter "0" always;
+	 * since "0" is the only informative variable, always the same value should be predicted 
+	 */
+	public void testINContNonActive() {
+		RegtreeBuildParams buildparams = new RegtreeBuildParams();
+		buildparams.random = new Random(3);
+		buildparams.splitMin = 1;
+		//theta 0 has two categorical values: 1,2; all others are continuous
+		int[] catDomainSizes = {2,1,0};
+		buildparams.catDomainSizes = catDomainSizes;
+		buildparams.minVariance = Math.pow(10,-14);
+		buildparams.ratioFeatures = 1;
+		
+		buildparams.nameConditionsMapOp = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsArray = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsValues = new HashMap<Integer, double[][][]>();
+		
+		// "0" | "1" in {2.0,3.0}
+		int[][] op = {{4}}; //IN
+		buildparams.nameConditionsMapOp.put(0, op);
+		int[][] par = {{1}}; //Parent first theta
+		buildparams.nameConditionsMapParentsArray.put(0, par);
+		double[][][] val = {{{2.0,3.0}}}; //value 2.0,3.0
+		buildparams.nameConditionsMapParentsValues.put(0, val);
+		
+		int numTrees = 10;
+		double[][] allTheta = {{1.,1.}, {2.,1.} };
+		
+		//no features
+		double[][] allX = {{0}, {0}};
+		int[][] theta_inst_idxs = {{0,0},{1,1},{0,1},{1,0}};
+		double[] y = {4., 1., 5., 2.};
+				
+	
+		RandomForest rf = RandomForest.learnModel(numTrees, allTheta, allX, theta_inst_idxs, y, buildparams);
+	
+		double[][] x = {{1.,1.,0.,}, {2.,0.,0.}, {1.,1.,1.}};
+		double[][] pred = RandomForest.apply(rf, x);
+		System.out.println("IN NonActive Test:");
+		System.out.println(Arrays.deepToString(pred));
+		//fail("Not yet implemented");
+		
+		assertTrue(pred[0][0] == 3.175);
+		assertTrue(pred[1][0] == 3.175);
+		assertTrue(pred[2][0] == 3.175);
+	}
+	
+	@Test
+	/*
+	 * test IN by adding a "in" condition that does nothing;
+	 */
+	public void testINCont() {
+		RegtreeBuildParams buildparams = new RegtreeBuildParams();
+		buildparams.random = new Random(3);
+		buildparams.splitMin = 1;
+		//theta 0 has two categorical values: 1,2; all others are continuous
+		int[] catDomainSizes = {2,1,0};
+		buildparams.catDomainSizes = catDomainSizes;
+		buildparams.minVariance = Math.pow(10,-14);
+		buildparams.ratioFeatures = 1;
+		
+		buildparams.nameConditionsMapOp = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsArray = new HashMap<Integer, int[][]>();
+		buildparams.nameConditionsMapParentsValues = new HashMap<Integer, double[][][]>();
+		
+		// "0" | "1" in {1.0}
+		int[][] op = {{4}}; //IN
+		buildparams.nameConditionsMapOp.put(0, op);
+		int[][] par = {{1}}; //Parent first theta
+		buildparams.nameConditionsMapParentsArray.put(0, par);
+		double[][][] val = {{{1.}}}; //value 0.0
+		buildparams.nameConditionsMapParentsValues.put(0, val);
+		
+		int numTrees = 10;
+		double[][] allTheta = {{1.,1.}, {2.,1.} };
+		
+		//no features
+		double[][] allX = {{0}, {0}};
+		int[][] theta_inst_idxs = {{0,0},{1,1},{0,1},{1,0}};
+		double[] y = {4., 1., 5., 2.};
+				
+	
+		RandomForest rf = RandomForest.learnModel(numTrees, allTheta, allX, theta_inst_idxs, y, buildparams);
+	
+		double[][] x = {{1.,1.,0.,}, {2.,0.,0.}, {1.,1.,1.}};
+		double[][] pred = RandomForest.apply(rf, x);
+		System.out.println("IN Test:");
+		System.out.println(Arrays.deepToString(pred));
+		//fail("Not yet implemented");
+		
+		assertTrue(pred[0][0] > 4.38);
+		assertTrue(pred[1][0] == 1.7);
+		assertTrue(pred[2][0] > 4.38);
 	}
 
 }
