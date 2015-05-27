d06110e5fecd2a58c56883bc598e5ad7ba438a73
Frank Hutter <hutter@cs.uni-freiburg.de>
2015-05-27 19:43:43 +0200

    Changed RfData and CsvToDataConverter a fair bit.

---
+++ src/ca/ubc/cs/beta/models/fastrf/utils/TripletDoubleArrayDoubleArrayDouble.java
@@ -0,0 +1,12 @@
+package ca.ubc.cs.beta.models.fastrf.utils;
+
+public class TripletDoubleArrayDoubleArrayDouble {
+	public TripletDoubleArrayDoubleArrayDouble(double[] theta, double[] x, double y) {
+		this.theta = theta;
+		this.x = x;
+		this.y = y;
+	}
+	public double[] theta;
+	public double[] x;
+	public double y;
+}
